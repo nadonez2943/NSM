4b2b0161a45f9811cfebb57ee032ccbee82f40ee
nadonez2943 <nadon.ez2943@gmail.com>
2022-09-23 16:19:30 +0700

    v.14

--- APP/main.py
+++ APP/main.py
@@ -488,6 +488,30 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
+#อัพเอกสาร
+@app.route('/doc', methods=['GET',"POST"])
+def doc():
+    form = UploadFileForm()
+    conn = None
+    cursor = None
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("SELECT * FROM nsm_project.path")
+    row = cursor.fetchall()
+    if form.validate_on_submit():
+       
+        file = form.file.data # First grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        sql = "INSERT INTO path(path_path) VALUES(%s)"
+        data = secure_filename(file.filename)
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute(sql, data)
+        conn.commit()
+        return "File has been uploaded."
+    return render_template('test.html', row=row,form=form)
+
 #ทดสอบ
 
 
