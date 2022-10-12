dd651afdd77c385d1db316ae791e66f230b2ef32
PeeraponYu <hipergamer01@gmail.com>
2022-10-12 17:07:41 +0700

    v.24.6

--- APP/main.py
+++ APP/main.py
@@ -872,7 +872,6 @@ def addProject():
         pj_financeAmount = request.form['financeAmount']
         pj_budgetSource = request.form['budgetSource']
         pj_budgetYears = request.form['budgetYears']
-        start_draft = request.form['start_draft']
         if  pj_refNumber and of_id and dv_id and pj_name and pj_type and pj_amount and pj_detail and pj_financeAmount and pj_budgetSource and pj_budgetYears and request.method == 'POST':
             sql1 = "INSERT INTO projects (pj_refNumber,of_id,dv_id,pj_name,pj_type,pj_amount,pj_detail,pj_financeAmount,pj_budgetSource,pj_budgetYears) VALUES(%s, %s, %s, %s,%s, %s, %s, %s, %s, %s)"
             data1 = (pj_refNumber,of_id,dv_id,pj_name,pj_type,pj_amount,pj_detail,pj_financeAmount,pj_budgetSource,pj_budgetYears)
@@ -884,8 +883,8 @@ def addProject():
             sql3 = "INSERT INTO manager (user_id,pj_id) VALUES(%s, %s)"
             data3 = (user,pj_id)
             cursor.execute(sql3, data3)
-            sql4 = "INSERT INTO process (pj_id,stdraft_id,stcon_id,stex_id,startproject_date,start_draft) VALUES(%s,%s,%s,%s,%s,%s)"
-            data4 = (pj_id,1,1,1,date.today(),start_draft)
+            sql4 = "INSERT INTO process (pj_id,stdraft_id,stcon_id,stex_id,startproject_date) VALUES(%s,%s,%s,%s,%s)"
+            data4 = (pj_id,1,1,1,date.today())
             cursor.execute(sql4, data4)
             conn.commit()
             return redirect ('/myproject')
