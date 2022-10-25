7969c37dfbc309fea05ef149dfa3abd416c6697e
PeeraponYu <hipergamer01@gmail.com>
2022-10-25 13:30:27 +0700

    f1

---
+++ APP/db_config.py
@@ -0,0 +1,12 @@
+from app import app
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '123456789'
+app.config['MYSQL_DATABASE_DB'] = 'nsm_project'
+app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+mysql.init_app(app)
+   
